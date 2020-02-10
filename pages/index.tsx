f425ea86b6f20e134e7601ad15a4f771884298c2
Marin Procureur <marin.procureur@gmail.com>
2020-02-10 20:25:27 +0100

    Async props loading on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
-import fetch from "isomorphic-unfetch";
 import { NextPage } from "next";
+import fetch from "isomorphic-unfetch";
 
-interface HomeProps {}
+interface HomeProps {
+  tickets: any;
+}
 
-const Home: NextPage<HomeProps> = _props => {
-  return <p></p>;
+const Home: NextPage<HomeProps> = props => {
+  return <p>{JSON.stringify(props.tickets)}</p>;
+};
+
+// fetch async data before loading the page
+// see : https://nextjs.org/learn/basics/fetching-data-for-pages
+Home.getInitialProps = async ctx => {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const tickets = await res.json();
+
+  return {
+    tickets
+  };
 };
 
 export default Home;
