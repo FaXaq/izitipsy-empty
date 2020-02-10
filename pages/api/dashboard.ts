c1628bdb9a321b4d20b1ee94f81a96efd3b049a2
Marin Procureur <marin.procureur@gmail.com>
2020-02-10 20:56:38 +0100

    Don't use getInitialProps as it slows the page rendering

--- pages/api/dashboard.ts
+++ pages/api/dashboard.ts
@@ -1,3 +1,9 @@
 import withDb from "../../utils/db";
+import Ticket from "../../schemas/tickets";
 
-export default withDb(async (req, res) => {});
+export default withDb(async (req, res) => {
+  Ticket.find({}, (_, tickets) => {
+    res.status(200);
+    res.send(tickets);
+  });
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,24 @@
 import { NextPage } from "next";
 import fetch from "isomorphic-unfetch";
+import { useState, useEffect } from "react";
 
-interface HomeProps {
-  tickets: any;
-}
+const Home: NextPage<{}> = () => {
+  const [tickets, setTickets] = useState([]);
 
-const Home: NextPage<HomeProps> = props => {
-  return <p>{JSON.stringify(props.tickets)}</p>;
-};
+  async function fetchTickets() {
+    const res = await fetch("/api/dashboard");
+    setTickets(await res.json());
+  }
 
-// fetch async data before loading the page
-// see : https://nextjs.org/learn/basics/fetching-data-for-pages
-Home.getInitialProps = async ctx => {
-  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
-  const tickets = await res.json();
+  useEffect(() => {
+    fetchTickets();
+  }, []);
 
-  return {
-    tickets
-  };
+  return (
+    <div>
+      <p>{JSON.stringify(tickets)}</p>
+    </div>
+  );
 };
 
 export default Home;
