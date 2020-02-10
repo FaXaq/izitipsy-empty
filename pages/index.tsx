22df58a7cc144fcde44edf0ad8c827de324a6cbf
Marin Procureur <marin.procureur@gmail.com>
2020-02-10 21:41:41 +0100

    Sort tickets by amounts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,46 @@ import fetch from "isomorphic-unfetch";
 import { useState, useEffect } from "react";
 
 const Home: NextPage<{}> = () => {
-  const [tickets, setTickets] = useState([]);
+  const [tickets, setTickets] = useState<any[]>([]);
+  const [staticFee, setStaticFee] = useState(0.3);
+  // Currently banking fee is a 2.9%
+  const [bankingFee, setBankingFee] = useState(0.029);
+  const [step, setStep] = useState(20);
+  const [amounts, setAmounts] = useState({});
 
-  async function fetchTickets() {
+  async function refreshTickets() {
     const res = await fetch("/api/dashboard");
-    setTickets(await res.json());
+    const unsortedTickets = await res.json();
+    setTickets(unsortedTickets);
   }
 
   useEffect(() => {
-    fetchTickets();
+    refreshTickets();
   }, []);
+
+  function sortTicketsByAmounts(tickets: any[]): any {
+    const steps: { [key: number]: any[] } = {};
+    tickets.map(t => {
+      // find the step of this ticket
+      const roundedStep = Math.ceil(t.amount / step);
+
+      // if no ticket has been at this step initialize it with empty array
+      if (!steps[roundedStep]) steps[roundedStep] = [];
+
+      // add ticket to this step
+      steps[roundedStep].push(t);
+    });
+    console.log(steps);
+    return steps;
+  }
+
+  useEffect(() => {
+    setAmounts(sortTicketsByAmounts(tickets));
+  }, [tickets, step]);
 
   return (
     <div>
-      <p>{JSON.stringify(tickets)}</p>
+      <p>{JSON.stringify(amounts)}</p>
     </div>
   );
 };
