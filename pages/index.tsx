f4c1bc83011be39d3aea5827a70e7b9362c5e413
Marin Procureur <marin.procureur@gmail.com>
2020-02-12 08:56:06 +0100

    [FIX] - Sort tickets by amount

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage<{}> = () => {
     const steps: { [key: number]: any[] } = {};
     tickets.map(t => {
       // find the step of this ticket
-      const roundedStep = Math.ceil(t.amount / step);
+      const roundedStep = Math.floor(t.amount / step);
 
       // if no ticket has been at this step initialize it with empty array
       if (!steps[roundedStep]) steps[roundedStep] = [];
